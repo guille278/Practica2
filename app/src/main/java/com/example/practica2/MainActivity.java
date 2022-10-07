7f99f7da79792cc7cc3d68edd5202d2844fd93d5
guille278 <guillermoal278@gmail.com>
2022-10-06 22:37:08 -0500

    Validacion de vacio

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -18,29 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gNum = (EditText) findViewById(R.id.gNum);
-        button = (Button) findViewById(R.id.button);
+        gNum = findViewById(R.id.gNum);
+        button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String value = gNum.getText().toString();
-                int numero = Integer.valueOf(value);
-                
                 if (value.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Debes ingresa un numero", Toast.LENGTH_SHORT).show();
-                }else{
-                    if(numero <=0){
+                } else {
+                    int numero = Integer.parseInt(value);
+                    if (numero <= 0) {
                         Toast.makeText(getApplicationContext(), "Ingresa un numero mayor que cero", Toast.LENGTH_SHORT).show();
-                    }else if(numero > 32000){
-                        Toast.makeText(getApplicationContext(), "Ingresa un nuermo menor", Toast.LENGTH_SHORT).show();
-                    }else{
+                    } else if (numero > 32000) {
+                        Toast.makeText(getApplicationContext(), "Ingresa un numero menor a 32,000", Toast.LENGTH_SHORT).show();
+                    } else {
                         Toast.makeText(getApplicationContext(), "Ingresaste el numero: " + numero, Toast.LENGTH_SHORT).show();
                     }
                 }
-
-
-
             }
         });
 
